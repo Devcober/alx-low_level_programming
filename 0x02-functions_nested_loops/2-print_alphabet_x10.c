5db2c204aacb0434e23ea7d504690555a4578553
devcober <125829731+Devcober@users.noreply.github.com>
2023-03-21 17:15:03 -0700

    add code files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ *print_alphabet_x10 - function that prints 10 times the alphabet in lowercase
+ *alphabet a to z x10
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 97;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
