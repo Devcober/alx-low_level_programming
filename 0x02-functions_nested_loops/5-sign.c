02a616ec15acffcd3abbca9bda60849a7538dc50
devcober <125829731+Devcober@users.noreply.github.com>
2023-03-22 08:37:10 -0700

    add code files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  * *
  * *@n: is the input of the function
  * *
- * Return:
+ * *Return:
  * *Returns 1 and prints + if n is greater than zero
  * *Returns 0 and prints 0 if n is zero
  * *Returns -1 and prints - if n is less than zero
@@ -15,17 +15,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 }
