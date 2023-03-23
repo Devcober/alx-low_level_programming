845a438a9821265394ebcd4d958bba192999e996
devcober <125829731+Devcober@users.noreply.github.com>
2023-03-22 19:03:11 -0700

    add code files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * * int add(int, int) - that adds two integers and returns the result
+ * * @a: is the input of the function
+ * * @b: is the input of the function
+ * * Return: return adds of two integers
+*/
+
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return (add);
+
+}
